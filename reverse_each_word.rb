4650035634f07965c3d623841a1de27f6a9fd5e1
JUAN M RESTREPO <tinto922@gmail.com>
2020-03-25 19:35:01 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -9,7 +9,7 @@ def reverse_each_word(string)
   intermidiate_array.join(" ")
 end 
 
-reverse_each_word(string)
+puts reverse_each_word(string)
 
 #puts "Hello there, and how are you?".reverse   does not work 
   #returns this ?uoy era woh dna ,ereht olleH
